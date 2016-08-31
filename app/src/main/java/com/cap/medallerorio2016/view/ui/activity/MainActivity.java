a1e9b29b294af805075629fe32d62d501abd2c5d
Christopher <ch.cardenas.pacheco@gmail.com>
2016-08-31 13:39:22 -0500

    Subida 6

--- app/src/main/java/com/cap/medallerorio2016/view/ui/activity/MainActivity.java
+++ app/src/main/java/com/cap/medallerorio2016/view/ui/activity/MainActivity.java
@@ -1,8 +1,6 @@
 package com.cap.medallerorio2016.view.ui.activity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
 
 import com.cap.medallerorio2016.R;
 import com.cap.medallerorio2016.di.HasComponent;
@@ -11,9 +9,6 @@ import com.cap.medallerorio2016.di.component.ListarMedalleroComponent;
 import com.cap.medallerorio2016.di.module.ListarMedalleroModule;
 import com.cap.medallerorio2016.view.ui.fragment.MainFragment;
 import com.cap.medallerorio2016.viewmodel.BaseMedalleroModel;
-import com.squareup.picasso.Picasso;
-
-import butterknife.BindView;
 
 /**
  * Created by CAP on 15/08/2016.
@@ -21,9 +16,6 @@ import butterknife.BindView;
 public class MainActivity extends BaseActivity implements HasComponent<ListarMedalleroComponent>,MainFragment.DatosMedalleroListener{
 
     private ListarMedalleroComponent listarMedalleroComponent;
-
-    /*@BindView(R.id.ivolimpiadasrio2016)
-    ImageView logoRio2016;*/
 
     @Override
     protected int getLayoutId() {
@@ -33,7 +25,6 @@ public class MainActivity extends BaseActivity implements HasComponent<ListarMed
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Picasso.with(this).load(R.drawable.rio2016).into(logoRio2016);
         initalizeInjector();
     }
 
